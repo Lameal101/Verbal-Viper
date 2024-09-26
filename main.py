849791896816c279088debc5cc883862822fcbff
Lameal101 <yash28112005sharma@gmail.com>
2024-09-26 15:40:35 +0530

    flaskadded

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("this is to check if this is verbal viper")
+from flask import Flask,render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/") #python decorators
+def hello_world():
+    return "<h2>this website is working</h2>"
+
+@app.route("/red")
+def tomatos():
+    return render_template("index.html")
+
+@app.route("/iron")
+def fan():
+    return "<h3>fans and breezes</h3>"
+
+if __name__ == "__main__":
+    app.run(debug = True, host = "0.0.0.0")
