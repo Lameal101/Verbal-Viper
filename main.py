17e86b52131b2e18e21b3aa6bfabc0f609381b4c
Lameal101 <yash28112005sharma@gmail.com>
2024-09-27 01:02:07 +0530

    firstPOST

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -7,6 +7,14 @@ CORS(app)
 @app.route("/") #python decorators
 def hello_world():
     return "<h2>this website is working</h2>"
+
+@app.route("/data",methods = ["GET","POST"])
+def data():
+    if request.method == "POST":
+        print(request.json)
+        print(request.json["title"])
+        return "succesfull"
+    return "<h1>HEAD</h1>"
 
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
