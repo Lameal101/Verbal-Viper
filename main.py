3927a264142f49914796d1b75960cae97d300213
Lameal101 <yash28112005sharma@gmail.com>
2024-09-27 01:09:45 +0530

    ImportedRapgenerator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,request
 from flask_cors import CORS
+from test import rapgenerator as rg
 
 app = Flask(__name__)
 CORS(app)
@@ -13,7 +14,8 @@ def data():
     if request.method == "POST":
         print(request.json)
         print(request.json["title"])
-        return "succesfull"
+        lyrics = rg(request.json["title"])
+        return lyrics
     return "<h1>HEAD</h1>"
 
 if __name__ == "__main__":
