258ae4e2219bf3a3cab8933f15486b6a2736dafa
Lameal101 <yash28112005sharma@gmail.com>
2024-09-26 22:58:22 +0530

    UnnecessaryRootsRemoved

--- main.py
+++ main.py
@@ -8,13 +8,5 @@ CORS(app)
 def hello_world():
     return "<h2>this website is working</h2>"
 
-@app.route("/red")
-def tomatos():
-    return render_template("index.html")
-
-@app.route("/iron")
-def fan():
-    return "<h3>fans and breezes</h3>"
-
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0")
